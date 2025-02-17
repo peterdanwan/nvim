1abf843324b2fdb6815fca250a321f634dadd017
peterdanwan <peterdanwan@gmail.com>
2025-02-16 20:56:20 -0500

    docs: update file comment

--- lua/peter/remaps/nvim-remap.lua
+++ lua/peter/remaps/nvim-remap.lua
@@ -1,8 +1,10 @@
--- lua/peter/remap.lua
+-- lua/peter/nvim-remap.lua
 
 -- Gets back to the netrc Explorer view 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Use Ctrl + q to enter Visual Block Mode?
-vim.keymap.set("n", '<C-q>', '<C-v>', { noremap = true })
+vim.keymap.set("n", "<C-q>", "<C-v>", { noremap = true })
 
+-- Show hover documentation
+vim.keymap.set({"n", "v"}, "gh", vim.lsp.buf.hover, { desc = "Show hover documentation" })
