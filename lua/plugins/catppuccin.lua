7fe7f1fce924807b655f1256746e625bc007c164
peterdanwan <peterdanwan@gmail.com>
2025-02-17 21:05:21 -0500

    fix: remove catppuccin transparent_background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,10 @@
-return { 
+return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function() -- Lazy's config function calls require("package").setup(opts) by default
     vim.cmd.colorscheme "catppuccin"
-    transparent_background = false
+    -- transparent_background = false
 -- Set transparent highlights AFTER colorscheme
    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
