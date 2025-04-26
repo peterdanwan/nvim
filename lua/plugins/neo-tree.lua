929eb86a373e07947df831908433f9952f02556a
peterdanwan <peterdanwan@gmail.com>
2025-04-26 14:49:47 -0400

    fix: return an empty object to prevent neotree on startup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,22 @@
 -- lua/plugins/neo-tree.lua
 
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree show<CR>', {})
-  end
-}
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
+--   branch = "v3.x",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-tree/nvim-web-devicons",
+--     "MunifTanjim/nui.nvim",
+--   },
+--   config = function()
+--     -- Configure neo-tree to not open automatically
+--     require("neo-tree").setup({
+--       window = {
+--         auto_open = false
+--       }
+--     })
+--     vim.keymap.set('n', '<C-b>', ':Neotree show<CR>', {})
+--   end
+-- }
+
+return {}
